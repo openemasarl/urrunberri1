0193e69109704f8a70fa43af0326ea36810517e0
matthewc00002 <oculiumllc@gmail.com>
2026-07-29 11:21:24 +0200

    feat: scripts admin install et desinstall rapides

---
+++ scripts/admin/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# UrrunBerri OS — Script d'installation rapide
+# Openema SARL — https://github.com/openemasarl/urrunberri1
+curl -fsSL https://raw.githubusercontent.com/openemasarl/urrunberri1/main/scripts/install.sh | bash && reboot
