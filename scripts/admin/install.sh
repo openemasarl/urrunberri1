91189a5006b5a095ad72fd5cd402de9ae19c2738
matthewc00002 <oculiumllc@gmail.com>
2026-07-30 09:16:17 +0200

    fix: scripts admin pointent vers branche test

--- scripts/admin/install.sh
+++ scripts/admin/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # UrrunBerri OS — Script d'installation rapide
 # Openema SARL — https://github.com/openemasarl/urrunberri1
-curl -fsSL https://raw.githubusercontent.com/openemasarl/urrunberri1/main/scripts/install.sh | bash && reboot
+curl -fsSL https://raw.githubusercontent.com/openemasarl/urrunberri1/test/scripts/install.sh | bash && reboot
